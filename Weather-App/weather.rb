0c743681e58381e8cc1829a470da3070bc533ecb
Rosana Rodrigues <rodrig8585@gmail.com>
2016-07-03 15:35:50 -0700

    updating

--- Weather-App/weather.rb
+++ Weather-App/weather.rb
@@ -1,38 +1,20 @@
-#final project for Skillcrush 104 Ruby Class
-#the purpose is to get the forecast for a location selected by the user
-
-
 require 'barometer'
-
-def get_weather(where)
-    barometer = Barometer.new(where)
-    weather = barometer.measure
-    
-    #today variabloe below used in forecasts loop in if statement
-    
-    today = Time.now.strftime("%d").to_i
-    tomorrow = today+1
-    
-#print today's weather
-    puts "Today's weather is  with a high of" + weather.today.high.f.to_s + " and a low of " + weather.today.low.f
-    
-    #print rest of forecast
-    weather.forecast.each do |forecast|
-        day = forecast.starts_at.dat
-        if day == today
-            day_name = "Today"
-        else
-            day_name = forecast.starts_at.strftime('%A')
-            puts day_name + "I will be" + forecast.icon + "with low of " + forecasts.low.fto_s + "and a high of " + forecast.high.f.to_s 
-        end #end for if statement
-        
-    end # end for loop
-    
- #get location from user
-    puts "What zip code do you need the temperature for?"
-    where = gets.chomp
-    get_weather(where)
-    
-    
-
-    
+ 
+def get_locations_weather(location)
+  Barometer.new(location).measure
+end
+ 
+weather = get_locations_weather('Chicago')
+tomorrow = Time.now.strftime('%d').to_i + 1
+ 
+weather.forecast.each do |forecast|
+  day = forecast.starts_at.day
+  
+  if day == tomorrow
+    dayName = 'Tomorrow'
+  else
+    dayName = forecast.starts_at.strftime('%A')
+  end
+ 
+    puts dayName + ' will be' + forecast.icon + ' with a low of' + forecast.low.f.to_s + ' and a high of ' + forecast.high.f.to_s
+end
--- nokogiri.rb
+++
@@ -1,8 +0,0 @@
-require 'nokogiri'
-require 'open-uri'
-
-doc = Nokogiri::HTML(open('http://www.marthastewart.com/312598/brick-pressed-sandwich'))
-
-puts doc
-
-puts doc.search("ingredients")
